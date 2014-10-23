4be62d23ae0de80523c2928d958843ea89eb6500
Don Park <donp@donp.org>
2014-10-23 09:48:02 -0700

    sql, api terraform files

---
+++ terraform/api.tf
@@ -0,0 +1,9 @@
+resource "digitalocean_droplet" "icecondor-api-nyc3-aa" {
+  name = "icecondor-api-nyc3-aa"
+  size = "512mb"
+  image = "ubuntu-14-04-x64"
+  region = "nyc3"
+  private_networking = true
+  ssh_keys = ["9d:79:da:b7:00:29:c4:1b:68:06:7d:bd:e2:99:90:42"]
+}
+
