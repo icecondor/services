63b6158f497ca463500b49894bde501795f9a06f
Don Park <donp@donp.org>
2018-08-06 15:21:55 -0700

    tf fixers

--- terraform/production/api.tf
+++ terraform/production/api.tf
@@ -1,9 +1,15 @@
 resource "digitalocean_droplet" "icecondor-api-nyc3-aa" {
-  name = "icecondor-api-nyc3-aa"
-  size = "512mb"
-  image = "ubuntu-14-04-x64"
-  region = "nyc3"
-  private_networking = true
-  ssh_keys = ["9d:79:da:b7:00:29:c4:1b:68:06:7d:bd:e2:99:90:42"]
-}
+  lifecycle { 
+   prevent_destroy = true
+   ignore_changes = "resize_disk"
+  }
 
+  ipv6               = "true"
+  name               = "mx.icecondor.com"
+  size               = "1gb"
+  image              = "6918990"                                           #"ubuntu-14-04-x64"
+  region             = "nyc3"
+  resize_disk        = false
+  private_networking = true
+  ssh_keys           = ["9d:79:da:b7:00:29:c4:1b:68:06:7d:bd:e2:99:90:42"]
+}
--- terraform/production/sql.tf
+++ terraform/production/sql.tf
@@ -1,8 +1,8 @@
-resource "digitalocean_droplet" "icecondor-sql-nyc3-aa" {
-  name = "icecondor-sql-nyc3-aa"
-  size = "512mb"
-  image = "ubuntu-14-04-x64"
-  region = "nyc3"
+resource "digitalocean_droplet" "icecondor-sql-nyc3-bb" {
+  name               = "icecondor-sql-nyc3-bb"
+  size               = "1gb"
+  image              = "ubuntu-16-04-x64"
+  region             = "nyc3"
   private_networking = true
-  ssh_keys = ["9d:79:da:b7:00:29:c4:1b:68:06:7d:bd:e2:99:90:42"]
+  ssh_keys           = ["9d:79:da:b7:00:29:c4:1b:68:06:7d:bd:e2:99:90:42"]
 }
