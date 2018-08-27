a8f65a629c32b69b7e35eb8f7421e9da0028c57d
Don Park <donp@donp.org>
2018-08-27 12:07:26 -0700

    50gb volume

--- terraform/production/api-cc.tf
+++ terraform/production/api-cc.tf
@@ -3,9 +3,17 @@ resource "digitalocean_droplet" "icecondor-api-cc" {
   size               = "s-1vcpu-1gb"
   image              = "ubuntu-18-04-x64"
   region             = "nyc3"
+  volume_ids         = ["${digitalocean_volume.geodata.id}"]
   private_networking = true
   ipv6               = true
   monitoring         = true
   ssh_keys           = ["48:7e:89:f4:c6:0c:ed:28:e8:31:16:87:ce:13:30:04"]
   tags               = ["${digitalocean_tag.api.id}", "${digitalocean_tag.web.id}"]
+}
+
+resource "digitalocean_volume" "geodata" {
+  region      = "nyc3"
+  name        = "geodata"
+  size        = 50
+  description = "icecondor customer data"
 }
