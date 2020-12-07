69fd86bd76dec77b3289fb9cd1070733cddb2653
Don P <donp@donp.org>
2020-12-07 13:32:31 -0600

    match tf config to deployed droplet

--- terraform/production/api-dd.tf
+++ terraform/production/api-dd.tf
@@ -1,13 +1,13 @@
 resource "digitalocean_droplet" "icecondor-api-dd" {
-  name               = "icecondor-api-dd"
+  name               = "mx.icecondor.com"
   size               = "s-1vcpu-1gb"
-  image              = "ubuntu-18-04-x64"
+  image              = "37208325" #"ubuntu-18-04-x64"
   region             = "nyc3"
-  volume_ids         = ["${digitalocean_volume.geodata.id}"]
+  volume_ids         = [digitalocean_volume.geodata.id]
   private_networking = true
   ipv6               = true
   monitoring         = true
-  ssh_keys           = ["48:7e:89:f4:c6:0c:ed:28:e8:31:16:87:ce:13:30:04"]
+#  ssh_keys           = ["48:7e:89:f4:c6:0c:ed:28:e8:31:16:87:ce:13:30:04"]
   tags               = ["api", "web"]
 }
 
--- terraform/production/volume-geodata.tf
+++ terraform/production/volume-geodata.tf
@@ -1,7 +1,6 @@
 
 resource "digitalocean_volume" "geodata" {
   region      = "nyc3"
-  name        = "geodata"
-  size        = 62
-  description = "icecondor geo data"
+  name        = "volume-nyc3-03"
+  size        = 61
 }
