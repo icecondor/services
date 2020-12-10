5d5c707d831d17139c1c4191e90021cd031f410e
Don P <donp@donp.org>
2020-12-10 14:41:54 -0600

    setup DNS after creation

--- terraform/staging/app-staging.tf
+++ terraform/staging/app-staging.tf
@@ -8,4 +8,11 @@ resource "digitalocean_droplet" "icecondor-app-staging" {
   monitoring         = true
   ssh_keys           = ["48:7e:89:f4:c6:0c:ed:28:e8:31:16:87:ce:13:30:04"] #digitalocean/2018
   tags               = ["api", "web"]
+
+  provisioner "local-exec" {
+    command = "../../he_net_dns staging.icecondor.com ${digitalocean_droplet.icecondor-app-staging.ipv4_address}"
+  }
+  provisioner "local-exec" {
+    command = "../../he_net_dns api.staging.icecondor.com ${digitalocean_droplet.icecondor-app-staging.ipv4_address}"
+  }
 }
